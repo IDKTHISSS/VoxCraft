c4bb6fa7de40a709c47c4a29edcaaf36568fd0f2
IDKTHIS <polireek@gmail.com>
2025-07-05 12:08:15 +0300

    fix cmake for start engine

---
+++ EntryPoint.cpp
@@ -0,0 +1,14 @@
+//
+// Created by IDKTHIS on 05.07.2025.
+//
+
+#include "Engine/Application/Application.h"
+
+
+int main(int argc, char* argv[]) {
+    // Initialize the application
+    Engine::Application app;
+
+    // Run the main loop of the application
+    app.Run();
+}
