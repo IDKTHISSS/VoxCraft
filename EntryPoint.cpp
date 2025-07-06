e34ab8d3d1a652a3acb1d967410b7997dd986d88
IDKTHIS <polireek@gmail.com>
2025-07-06 07:52:39 +0300

    Added dev branch

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -7,7 +7,7 @@
 
 int main(int argc, char* argv[]) {
     // Initialize the application
-    Engine::Application app;
+    Engine::Application app("VoxCraft Beta", 800, 600);
 
     // Run the main loop of the application
     app.Run();
